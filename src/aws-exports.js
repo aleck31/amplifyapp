7a5963ca709dc71aba9a3cb10ec3b49cc716c039
aleck <me@myners.net>
2021-11-05 18:08:58 +0800

    add storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -28,7 +28,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://lvkre3cw3bg3hlqikljvimp2uy.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-idtxlvvjhvh4jbexgcnlkslfly"
+    "aws_appsync_apiKey": "da2-idtxlvvjhvh4jbexgcnlkslfly",
+    "aws_user_files_s3_bucket": "imgstore779180616-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
